af33d90c116e4c90708970f4d81124ad0b710bd8
donggi <devdonggilee@gmail.com>
2022-01-15 23:35:06 +0900

    [동기] - ch1 제출 (#10)
    * Add Container class 예제코드 추가
    
    - 책의 예제 코드를 따라 쳐보았습니다
    - 변수명이 a, b, c 처럼 되어있어 가독성이 좋지 않습니다.
    
    * Add Main class
    
    - 예제 코드를 실행시켜보기 위해 Main 클래스를 추가해주었습니다.
    - 컨테이너 a, b를 연결했을 때와 b, c를 연결했을 때 예상했던 것처럼 값이
      잘 출력되네요.
    
    * Add ch 1 요약 정리 리드미 작성
    
    - ch 1에 있는 내용을 요약 정리 해보았습니다
    
    * Add 리드미에 질문 추가
    
    - 해당 챕터를 읽고 궁금한 점 리드미에 추가
    
    * Add 예제 코드를 객체 지향적이게 작성해보았다
    
    - 같은 수조를 다시 연결할 수 없게 HashSet을 이용했다.
    - 컨테이너가 추가되면 수조 하나에 담길 물의양을 newAmount 변수로
      지정했고, newAmount 값을 가져오는 작업을 캡슐화 해보았다.
    - 코드를 작성해보며 질문할 부분을 주석처리했다.
    
    * Add PR 필요한 부분 리드미 작성
    
    - pr이 필요한 부분을 리드미에 작성했다

---
+++ Ch1/동기/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Container a = new Container();
+        Container b = new Container();
+        Container c = new Container();
+        Container d = new Container();
+
+        a.addWater(8);
+        b.addWater(5);
+
+        a.connectTo(b);
+        System.out.println(a.getAmount());
+        System.out.println(b.getAmount());
+
+        b.connectTo(c);
+
+        System.out.println(a.getAmount());
+        System.out.println(b.getAmount());
+        System.out.println(c.getAmount());
+    }
+}
